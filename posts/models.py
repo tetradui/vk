f1c4a003dda2013c4daf7d35858d53c431077390
nur950960 <yanshanlo2006@gamil.com>
2024-05-14 16:11:18 +0600

    post

---
+++ posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name='Автор')
+
+    title = models.CharField(max_length=100, verbose_name='Название', blank=True)
+    image = models.ImageField(upload_to='posts_img/', blank=True, verbose_name='Фото')
+
+    description = models.TextField(blank=True, verbose_name='Описание')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
---
+++ posts/views.py
@@ -0,0 +1,36 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+from .models import Post
+from .serializers import PostSerializers, PostListSerializers
+from .permissions import IsOwnerOrReadOnly
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['author']
+    search_fields = ['title']
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [AllowAny]
+        elif self.action in ['create']:
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsOwnerOrReadOnly]
+        return [permission() for permission in self.permission_classes]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializers
+        return self.serializer_class
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+
+
