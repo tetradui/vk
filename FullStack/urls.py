24745eb82971b4c381def415cfb918b158c29c9f
Kutman Almazbekov <kutmanalmazbekov@MacBook-Air-Kutman.local>
2024-05-16 13:12:20 +0600

    swagger

--- FullStack/urls.py
+++ FullStack/urls.py
@@ -17,9 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Vkontakte",
+        description="Social media site",
+        default_version="v1"
+    ),
+    public=True
+
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('account.urls')),
     path('api/v1/', include('posts.urls')),
+    path('api/v1/docs/', schema_view.with_ui('swagger'))
 
 ]
