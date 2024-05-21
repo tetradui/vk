308405e74e14061dc090cd0a45bba8f1d72f0e76
Kutman Almazbekov <kutmanalmazbekov@MacBook-Air-Kutman.local>
2024-05-21 18:58:59 +0600

    profile edit correct

--- account/serializers.py
+++ account/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
 from .utils import send_activation_code
+from .models import Profile
 
 
 User = get_user_model()
@@ -32,7 +33,35 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email', 'is_active']  # указываем нужные поля
 
+# class UserProfileSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id','first_name', 'last_name', 'email', 'pro_photo',)
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'location', 'birth_date')
+
 class UserProfileSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ('id','first_name', 'last_name', 'email', 'pro_photo',)
+        fields = ('id', 'first_name', 'last_name', 'email', 'pro_photo', 'phone', 'profile')
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop('profile', {})
+        profile = instance.profile
+
+        # Update user fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Update profile fields
+        for attr, value in profile_data.items():
+            setattr(profile, attr, value)
+        profile.save()
+
+        return instance
